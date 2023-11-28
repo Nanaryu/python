63907e2b639b866002a93fa81e81b355c8ff7c00
Nanaryu <112709452+Nanaryu@users.noreply.github.com>
2023-11-28 17:17:53 +0100

    Add files via upload

--- 28.11.23/231.py
+++ 28.11.23/231.py
@@ -10,14 +10,16 @@ try:
     if os.stat("dict.txt").st_size == 0:
         inx = set(inx)
         for word in inx:
-            dictA.write(word.lower() + "\n")
+            if len(word) > 1:
+                dictA.write(word.lower() + "\n")
     else:
         dictRv = dictR.read()
         dictRv = dictRv.split("\n")
         not_in_dict = set(inx) - set(dictRv)
         not_in_dict = set(not_in_dict)
         for word in not_in_dict:
-            dictA.write(word.lower() + "\n")
+            if len(word) > 1:
+                dictA.write(word.lower() + "\n")
 except Exception as e:
     print(f"ERROR: {e}")
     
