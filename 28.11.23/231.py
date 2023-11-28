eeae42eccdfb5e73b0f29bac1d7c68dd59d3d68e
Nanaryu <112709452+Nanaryu@users.noreply.github.com>
2023-11-28 16:48:01 +0100

    Add files via upload

--- 28.11.23/231.py
+++ 28.11.23/231.py
@@ -1,4 +1,20 @@
-f = open("in.txt", "r", encoding="utf-8")
-txt = f.read()
-dict = open("dict.txt", "a")
-dictxt = dict.read()
+import os
+inf = open("in.txt", "r", encoding="utf-8")
+inx = inf.read()
+inx = inx.split("\n")
+dictA = open("dict.txt", "a", encoding="utf-8")
+dictR = open("dict.txt", "r", encoding="utf-8")
+try:
+    if os.stat("dict.txt").st_size == 0:
+        for word in inx:
+            dictA.write(word + "\n")
+    else:
+        dictRv = dictR.read()
+        dictRv = dictRv.split("\n")
+        not_in_dict = set(inx) - set(dictRv)
+        not_in_dict = set(not_in_dict)
+        for word in not_in_dict:
+            dictA.write(word + "\n")
+except Exception as e:
+    print(f"ERROR: {e}")
+    
