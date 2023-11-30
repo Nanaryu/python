766e7030c8cb1c9c198ec05b81c2d001f4bdbe96
Nanaryu <filipqqq1234@gmail.com>
2023-11-30 18:56:54 +0100

    made it work

--- LCK_autocorrect/main.py
+++ LCK_autocorrect/main.py
@@ -1,59 +1,28 @@
-n = int(input())
-
-for _ in range(n):
-    d_w = input()
-    d_w.split(" ")
-    d = d_w[0]
-    w = d_w[2]
-    dic = []
-    mis = []
-    for i in range(int(d)):
-        dic.append(input())
-    for i in range(int(w)):
-        mis.append(input())
-    for word in mis: # word in misspelled
-        hindexw = []
-        for corword in dic: # correct word in dictionary
-            hamming = 0
-            ct = list(corword) # single letters of correct word
-            wt = list(word) # single letters of word
-            lct = len(ct)
-            lwt = len(wt)
-            max_len = max(lct, lwt)
-
-            if lct < max_len:
-                ct.extend([''] * (max_len - lct))
-            elif lwt < max_len:
-                wt.extend([''] * (max_len - lwt))
-
-            if lct > lwt:
-                for i, val in enumerate(wt):
-                    if val != ct[i]:
-                        hamming += 1
-            else:
-                for i, val in enumerate(ct):
-                    if val != wt[i]:
-                        hamming += 1
-            hindexw.append(hamming)
-        print(hindexw, dic[hindexw.index(min(hindexw))])
-
-'''input
-1 
-3 5
-computer
-mouse
-program
-konpuder
-house
-compoooo
-anagram
-oeifeln
-'''
-'''output
-computer
-mouse
-computer
-program
-mouse <---- SHOULD BE program
-'''
-#help
+def how_close(correct, sample): 
+    distance = 0
+    for c, s in zip(correct, sample):
+        if c != s:
+            distance += 1
+    return distance
+for _ in range(int(input())): 
+    dict_len, words_len = map(int, input().split())
+    dct = dict()
+    for i in range(dict_len):
+        word = input()
+        length = len(word)
+        if length in dct.keys(): 
+            dct[length].append(word)
+        else:
+            dct[length] = [word]
+answer = []
+for i in range(words_len):
+    word = input()
+    best_match = len(word)
+    for correct_word in dct[len(word)]:
+        dist = how_close(correct_word, word)
+        if dist < best_match:
+            best_match = dist
+            best_word = correct_word
+    answer.append(best_word)
+for word in answer:
+    print(word)
