23c35029d175b96e7c90a72988dda787c678e4af
Nanaryu <112709452+Nanaryu@users.noreply.github.com>
2024-10-31 16:22:19 +0100

    Update cs.cs

--- cs.cs
+++ cs.cs
@@ -13,6 +13,22 @@ namespace MauiApp2
             var control = sender as Button;
             field.Text += control.Text;
         }
+        private void clear(object sender, EventArgs e)
+        {
+            field.Text = "0";
+        }
+        private void plus(object sender, EventArgs e)
+        {
+            prev.Text += " + " + field.Text;
+            operation.Text = "+";
+        }
+        private void equal(object sender, EventArgs e)
+        {
+            int x1, x2;
+            int.TryParse(field.Text, out x1);
+            int.TryParse(prev.Text, out x1);
+            field.Text = x1 + x2;
+        }
     }
 
 }
